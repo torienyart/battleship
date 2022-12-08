23612e25b43f9b729433e8a36de3b70f85813cf5
Melony Franchini <meltravelz@gmail.com>
2022-12-07 18:30:15 -0700

    feat: Add board initialize & refactor: Board has cells test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,15 +14,12 @@ describe Board do
         end 
 
         it "has cells" do
-            expect(board.cells).to eq("A1", "A2", "A3", "A4", 
-                                    "B1", "B2", "B3", "B4", 
-                                    "C1", "C2", "C3", "C4", 
-                                    "D1", "D2", "D3", "D4")
+            expect(board.cells).to be_an_instance_of(Hash)
         end
     end
 
     describe "Validating Coordinates" do
-        it "can validate a coordinate" do
+        xit "can validate a coordinate" do
             expect(board.valid_coordinate?("A1")).to eq(true)
             expect(board.valid_coordinate?("D4")).to eq(true)
             expect(board.valid_coordinate?("A5")).to eq(false)
@@ -32,39 +29,39 @@ describe Board do
     end 
 
     describe "Validating Placements" do
-        it "determines if the placement of the ship is on the correct length of cells" do
+        xit "determines if the placement of the ship is on the correct length of cells" do
             expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
             expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
         end
 
-        it "ensures the placement of the ship is on consecutive cells" do
+        xit "ensures the placement of the ship is on consecutive cells" do
             expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
             expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
             expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
             expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
         end
 
-        it "ensures the placement of the ship is down or across (not diagonal)" do
+        xit "ensures the placement of the ship is down or across (not diagonal)" do
             expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
             expect(board.valid_placement?(submarine, ["C2", "D3"])).to equal(false)
         end
 
-        it "can determine a valid placement" do
+        xit "can determine a valid placement" do
             expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
             expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
         end
     end
 
     describe "Placing Ships" do
-        it "can recognize one individual cell" do
+        xit "can recognize one individual cell" do
             board.place(cruiser, ["A1", "A2", "A3"])
 
             expect(cell_1 = board.cells["A1"]).to be_an_instance_of(Cell)
             expect(cell_2 = board.cells["A2"]).to be_an_instance_of(Cell)
-            expect(cell_3 = board.cells["A3"])..to be_an_instance_of(Cell)
+            expect(cell_3 = board.cells["A3"]).to be_an_instance_of(Cell)
         end
         
-        it "can place a ship in multiple cells" do
+        xit "can place a ship in multiple cells" do
             board.place(cruiser, ["A1", "A2", "A3"])
         
             expect(cell_1.ship).to eq(cruiser)
@@ -72,7 +69,7 @@ describe Board do
             expect(cell_3.ship).to eq(cruiser)
         end
 
-        it "confirms same ship can be in multiple cells at once" do
+        xit "confirms same ship can be in multiple cells at once" do
             board.place(cruiser, ["A1", "A2", "A3"])
             cell_1 = board.cells["A1"]
             cell_2 = board.cells["A2"]
@@ -84,7 +81,7 @@ describe Board do
     end 
 
     describe "Overlapping Ships" do
-        it "can identify if ship is already on a cell" do
+        xit "can identify if ship is already on a cell" do
             board.place(cruiser, ["A1", "A2", "A3"])
         
             expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
@@ -92,7 +89,7 @@ describe Board do
     end 
 
     describe "Rendering the Board" do
-        it "renders a 'string' representation of itself for the other player" do
+        xit "renders a 'string' representation of itself for the other player" do
             board.place(cruiser, ["A1", "A2", "A3"])    
 
             expect(board.render).to eq("  1 2 3 4 \n
@@ -102,7 +99,7 @@ describe Board do
                                         D . . . . \n")
         end
 
-        it "renders a TRUE 'string' representation of itself for player's own board" do
+        xit "renders a TRUE 'string' representation of itself for player's own board" do
             board.place(cruiser, ["A1", "A2", "A3"])    
 
             expect(board.render(true)).to eq("  1 2 3 4 \n
