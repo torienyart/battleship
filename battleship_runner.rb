59b184450864ca22d3f60d1d52bf4e232fe1e824
Melony Franchini <meltravelz@gmail.com>
2022-12-10 17:08:43 -0700

    test: Use runner file & Update print statements

---
+++ battleship_runner.rb
@@ -0,0 +1,7 @@
+require "./lib/ship"
+require "./lib/cell"
+require "./lib/board"
+require "./lib/game"
+
+game = Game.new
+game.start
--- lib/game.rb
+++ lib/game.rb
@@ -19,11 +19,11 @@ class Game
 # require 'pry'; binding.pry
 
     def start
-        p "I have laid out my ships on the grid.\n"
+        puts "I have laid out my ships on the grid.\n"
         "You now need to lay out your two ships.\n"
         "The Cruiser is three units long and the Submarine is two units long.\n"
         
-        p @p_board.render(false)
+        puts @p_board.render(false)
         
         # c_ship_placement
         p_ship_placement
@@ -42,7 +42,7 @@ class Game
 
     def p_ship_placement
     #Cruiser_Placement
-        p "Enter the squares for the Cruiser (3 spaces):\n> "
+        puts "Enter the squares for the Cruiser (3 spaces):\n> "
         
         #<while true do> would also work here (this is hard coding)
        loop do 
@@ -52,14 +52,14 @@ class Game
                 p_board.place(cruiser, user_input)
                 break 
             else
-                p "Those are invalid coordinates. Please try again:\n> "
+                puts "Those are invalid coordinates. Please try again:\n> "
             end
         end
         
-        p p_board.render(true)
+        puts p_board.render(true)
     
     #Sumbarine_placement
-        p "Enter the squares for the Submarine (2 spaces):\n> "
+        puts "Enter the squares for the Submarine (2 spaces):\n> "
             
         loop do
             user_input = gets.chomp.upcase
@@ -68,11 +68,11 @@ class Game
                 p_board.place(submarine, user_input) 
                 break
             else
-                p "Those are invalid coordinates. Please try again:\n> "
+                puts "Those are invalid coordinates. Please try again:\n> "
             end
         end
 
-        p p_board.render(true)
+        puts p_board.render(true)
     end
 
 end
