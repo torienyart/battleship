c0e9ff7868a89bdf082d5a88de0200ff8ed99833
Tori Enyart <torienyart@gmail.com>
2022-12-13 11:37:08 -0700

    fix: Clean up commented out lines

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -8,25 +8,3 @@ require "./lib/interface"
 interface = Interface.new
 
 interface.run_game
-# game = Game.new
-# two_player = TwoPlayerGame.new
-# puts "*======================================================*"
-# puts
-# puts
-# puts "Ahoy mateys! Test your wits at BATTLESHIP — a game of the seven seas.\n" +
-# "Enter 1 to play against the computer, enter 2 to play against a partner, or enter q to quit"
-
-# game_type = gets.chomp
-
-# loop do
-#     if game_type == '1' || game_type.downcase == "one"
-#         game.start
-#     elsif game_type == '2' || game_type.downcase == "two"
-#         two_player.start
-#     elsif game_type.downcase == 'q'
-#         break
-#     else
-#         puts "Yargh, I didn't understsand that. Make up your mind! I'm a very busy pirate!\n" +
-#         "Enter 1 to play against the computer, enter 2 to play against a partner, or enter q to quit"
-#     end
-# end
